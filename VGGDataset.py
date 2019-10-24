d37f1a4848e0963cb872d7d7f55b4dacad81bd10
barret-p <barretp@gmail.com>
2019-10-23 23:36:07 -0500

    create vgg classes

---
+++ VGGDataset.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import tensorboardX
+
+class VGGDataset:
+
+    def __init__(self):
+        pass
---
+++ VGGRunner.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import tensorboardX
+import torchvision.models as models
+from VGGNetwork import VGGNetwork
+
+class VGGRunner:
+    def __int__(self, features, num_classes):
+        self.model = VGGNetwork(features, num_classes)
+
+    def run():
+        pass
+    
+    def train():
+        pass
+
+    def validate():
+        pass
