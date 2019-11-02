3d2aa40522a51fbfaca7edd47ab29d62c02fd71f
Ankit Ramchandani <ankit61@tamu.edu>
2019-11-01 19:24:00 -0500

    removing deepcopy

--- src/HWGANDataset.py
+++ src/HWGANDataset.py
@@ -23,10 +23,9 @@ class CoordinatesToDeltaTransform(object):
         delta_datapoints = [(0, 0, 0)]
         for i in range(1, len(datapoints)):
             delta_datapoints.append((datapoints[i][0] - datapoints[i-1][0], datapoints[i][1] - datapoints[i-1][1], datapoints[i][2]))
-        
-        new_sample = copy.deepcopy(sample)
-        new_sample['datapoints'] = torch.tensor(delta_datapoints, dtype=torch.float)
-        return new_sample
+
+        sample['datapoints'] = torch.tensor(delta_datapoints, dtype=torch.float)
+        return sample
 
 class LineToOneHotMatrixTransform(object):
     """Converts a string of line_text into a one hot matrix
@@ -61,10 +60,9 @@ class LineTextToIntegerTransform(object):
         for i, c in enumerate(line_text):
             line_text_integers[i] = self.char_to_idx_map[c]
         
-        new_sample = copy.deepcopy(sample)
-        new_sample['line_text_integers'] = torch.tensor(line_text_integers, dtype=torch.long)
-        new_sample.pop('line_text', None)
-        return new_sample
+        sample['line_text_integers'] = torch.tensor(line_text_integers, dtype=torch.long)
+        sample.pop('line_text', None)
+        return sample
 
 class PadDatapointsTransform(object):
     """Pads the list of datapoints (x, y, p) to max_line_points constants."""
@@ -80,11 +78,8 @@ class PadDatapointsTransform(object):
         assert (self.max_line_points - len(datapoints)) >= 0, f'max_line_points ({self.max_line_points}) must be larger or equal to length of datapoints ({len(datapoints)})'
 
         padding = torch.tensor([(0, 0, 0)] * (self.max_line_points - len(datapoints)), dtype=torch.float)
-        padded_datapoints = torch.cat((datapoints, padding), dim=0)
-        
-        new_sample = copy.deepcopy(sample)
-        new_sample['datapoints'] = padded_datapoints
-        return new_sample
+        sample['datapoints'] = torch.cat((datapoints, padding), dim=0)
+        return sample
 
 class PadLineTextTransform(object):
     """Pads the string of line_text into a string of length max_line_text_length
@@ -101,11 +96,8 @@ class PadLineTextTransform(object):
 
         assert (self.max_line_text_length - len(line_text)) >= 0, f'max_line_text_length ({self.max_line_text_length}) must be larger or equal to line_text_length ({len(line_text)}) of datapoints'
         
-        new_line_text = line_text + self.pad_character * (self.max_line_text_length - len(line_text))
-        
-        new_sample = copy.deepcopy(sample)
-        new_sample['line_text'] = new_line_text
-        return new_sample
+        sample['line_text'] = line_text + self.pad_character * (self.max_line_text_length - len(line_text))
+        return sample
 
 class HWGANDataset(Dataset):
     """
