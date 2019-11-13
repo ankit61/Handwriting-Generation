4823769dec0ba4e08fbb486eb752b71f791d9019
Ankit Ramchandani <ankit61@tamu.edu>
2019-11-13 13:16:55 -0600

    making generator loss a module, adding teacher forcing

--- src/SupervisedGeneratorRunner.py
+++ src/SupervisedGeneratorRunner.py
@@ -8,22 +8,57 @@ import torch.nn.utils.rnn as rnn_utils
 import constants
 import torch
 from tqdm import tqdm
+from BaseModule import BaseModule
+import numpy as np
 
-LR = 0.005
+LR = 0.05
 MOMENTUM = 0.9
 WEIGHT_DECAY = 0
 GRADIENT_CLIP_NORM = 5
-UPDATE_BATCHES_PERIOD = 20
+UPDATE_BATCHES_PERIOD = 1
+
+class GeneratorLoss(BaseModule):
+    def __init__(self, writer, model, debug=True):
+        super(GeneratorLoss, self).__init__(debug)
+        self.writer = writer
+        self.model  = model
+
+    def forward(self, generated, gt, global_step):
+        xys  = generated.narrow(1, 0, 2)
+        ps   = generated.narrow(1, 2, 1)
+
+        mse_loss = nn.L1Loss().cuda() if torch.cuda.is_available() else nn.MSELoss()
+        bce_loss = nn.BCEWithLogitsLoss().cuda() if torch.cuda.is_available() else nn.BCEWithLogitsLoss()
+
+        loss_vals_weights = {
+            'mse': (mse_loss(xys, gt.narrow(1, 0, 2)), 1),
+            'bce': (bce_loss(ps, gt.narrow(1, 2, 1)), 22),
+            'invariant_regularization': (self.model.invariant.weight.norm(), 0.05)
+        }
+
+        final_loss_val = sum([v[0] * v[1] for k, v in loss_vals_weights.items()])
+
+        if self.writer != None:
+            for loss_name, loss_val_weight in loss_vals_weights.items():
+                loss_proportion = loss_val_weight[0] * loss_val_weight[1] / final_loss_val
+                self.writer.add_scalar(f'Generator/loss_proportion/{loss_name}', loss_proportion, global_step=global_step)
+
+        return final_loss_val
 
 class SupervisedGeneratorRunner(BaseRunner):
     def __init__(self, debug = True):
         model = GeneratorCell()
         optimizer = optim.Adam(model.parameters(), lr=LR)
+        loss_fn = GeneratorLoss(None, model)
         self.global_step = 0
+        self.force_teach_probability = 1
+        
         super(SupervisedGeneratorRunner, self).__init__(models=[model],
-            loss_fn=SupervisedGeneratorRunner.generator_loss, 
+            loss_fn=loss_fn, 
             optimizers=[optimizer], best_metric_name='loss', 
             should_minimize_best_metric=True)
+        
+        self.loss_fn.writer = self.writer
         self.set_gpu_keys(['datapoints', 'writer_id', 'line_text_integers'])
 
     def run_batch_and_get_metrics(self, batch, is_train_mode):
@@ -51,18 +86,27 @@ class SupervisedGeneratorRunner(BaseRunner):
             #do forward pass
             letter_id_sequences = batch['line_text_integers'][:cur_batch_size, :]
             writer_ids = batch['writer_id'][:cur_batch_size]
+            gt = packed_datapoints.data[batch_start:batch_start + cur_batch_size, :]
 
             for j in range(constants.LSTM_DEPTH):
                 last_hidden_and_cell_states[j] = (last_hidden_and_cell_states[j][0][:cur_batch_size, :], 
                                                   last_hidden_and_cell_states[j][1][:cur_batch_size, :])
 
+            if(np.random.rand() < self.force_teach_probability):
+                new_hidden = torch.zeros(last_hidden_and_cell_states[-1][0].shape)
+                new_hidden[:, 3:] = last_hidden_and_cell_states[-1][0][:, 3:]
+                new_hidden[:, :2] = gt.narrow(1, 0, 2)
+                new_hidden[:, 2]  = gt.narrow(1, 2, 1)
+                last_hidden_and_cell_states = last_hidden_and_cell_states[:-1] + \
+                        [(new_hidden, last_hidden_and_cell_states[-1][1])]
+
             last_hidden_and_cell_states = self.nets[0](writer_ids, letter_id_sequences,
                                                     last_hidden_and_cell_states)
 
             #compute loss
-            gt = packed_datapoints.data[batch_start:batch_start + cur_batch_size, :]
+
             generated = last_hidden_and_cell_states[-1][0][:, :3]
-            loss += self.loss_fn(generated, gt, self.writer, self.global_step)
+            loss += self.loss_fn(generated, gt, self.global_step)
             if is_train_mode:
                 #calculate gradients but don't update
                 loss.backward(retain_graph=(i < len(packed_datapoints.batch_sizes) - 1))
@@ -131,29 +175,3 @@ class SupervisedGeneratorRunner(BaseRunner):
 
         delta_points_to_image(generated_delta_points, constants.INTERMITTENT_OUTPUTS_BASE_DIR, f'output_{global_step}.png')
         delta_points_to_image(gt_delta_points, constants.INTERMITTENT_OUTPUTS_BASE_DIR, f'ground_truth_{global_step}.png')
-
-    @staticmethod
-    def generator_loss(generated, gt, writer=None, global_step=0):
-        xys  = generated.narrow(1, 0, 2)
-        ps   = generated.narrow(1, 2, 1)
-
-        #print(xys)
-        #print(ps)
-
-        mse_loss = nn.L1Loss().cuda() if torch.cuda.is_available() else nn.MSELoss()
-        bce_loss = nn.BCEWithLogitsLoss().cuda() if torch.cuda.is_available() else nn.BCEWithLogitsLoss()
-
-        mse_loss_val = mse_loss(xys, gt.narrow(1, 0, 2))
-        bce_loss_val = bce_loss(ps, gt.narrow(1, 2, 1))
-        final_loss_val = mse_loss_val + bce_loss_val
-
-        loss_proportions = {
-            'mse': mse_loss_val / final_loss_val * 100.0,
-            'bce': bce_loss_val / final_loss_val * 100.0
-        }
-
-        if writer != None:
-            for loss_type in loss_proportions:
-                writer.add_scalar(f'Generator/loss_proportion/{loss_type}', loss_proportions[loss_type], global_step=global_step)
-
-        return final_loss_val
